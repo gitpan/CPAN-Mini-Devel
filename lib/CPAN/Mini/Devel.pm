f70586703c4d62ad323f268a8c0de36509cf1940
David Golden <dagolden@cpan.org>
2014-02-26 23:57:54 +0000

    Import of DAGOLDEN/CPAN-Mini-Devel-0.04 from CPAN.
    gitpan-cpan-distribution: CPAN-Mini-Devel
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         DAGOLDEN/CPAN-Mini-Devel-0.04.tar.gz
    gitpan-cpan-author:       DAGOLDEN
    gitpan-cpan-maturity:     released

--- lib/CPAN/Mini/Devel.pm
+++ lib/CPAN/Mini/Devel.pm
@@ -1,13 +1,14 @@
-package CPAN::Mini::Devel;
 use 5.006;
 use strict;
 use warnings;
-our $VERSION = '0.03'; 
-$VERSION = eval $VERSION; ## no critic
+
+package CPAN::Mini::Devel;
+# ABSTRACT: Create CPAN::Mini mirror with developer releases
+our $VERSION = '0.04'; # VERSION
 
 use Config;
-use CPAN::Mini;
-use CPAN (); 
+use CPAN::Mini 0.567;
+use CPAN 1.92 ();
 use CPAN::Tarzip;
 use CPAN::HandleConfig;
 use File::Temp 0.20;
@@ -21,9 +22,8 @@ our @ISA = 'CPAN::Mini';
 # globals
 #--------------------------------------------------------------------------#
 
-my $tmp_dir = File::Temp->newdir( 'CPAN-Mini-Devel-XXXXXXX', 
-    DIR => File::Spec->tmpdir,
-);
+my $tmp_dir =
+  File::Temp->newdir( 'CPAN-Mini-Devel-XXXXXXX', DIR => File::Spec->tmpdir, );
 
 #--------------------------------------------------------------------------#
 # Extend index methods to miror find-ls.gz
@@ -33,7 +33,7 @@ my $index_file = 'indices/find-ls.gz';
 
 sub _fixed_mirrors {
     my $self = shift;
-    return ($index_file, $self->SUPER::_fixed_mirrors);
+    return ( $index_file, $self->SUPER::_fixed_mirrors );
 }
 
 #--------------------------------------------------------------------------#
@@ -41,19 +41,14 @@ sub _fixed_mirrors {
 #--------------------------------------------------------------------------#
 
 sub _get_mirror_list {
-	my $self  = shift;
+    my $self = shift;
 
     ## CPAN::Mini::Devel addition using find-ls.gz
-    my $file_ls =  File::Spec->catfile(
-        $self->{scratch},
-        qw(indices find-ls.gz)
-    );
+    my $file_ls = File::Spec->catfile( $self->{scratch}, qw(indices find-ls.gz) );
 
-    my $packages = File::Spec->catfile(
-        $self->{scratch},
-        qw(modules 02packages.details.txt.gz)
-    );
-    
+    my $packages =
+      File::Spec->catfile( $self->{scratch}, qw(modules 02packages.details.txt.gz) );
+
     return $self->_parse_module_index( $packages, $file_ls );
 }
 
@@ -73,12 +68,21 @@ my $module_index_re = qr{
     (\S+)                       # capture month 
     \s+                         # skip spaces
     (\S+)                       # capture year
-}xms; 
+}xms;
 
-my %months = ( 
-    Jan => '01', Feb => '02', Mar => '03', Apr => '04', May => '05',
-    Jun => '06', Jul => '07', Aug => '08', Sep => '09', Oct => '10',
-    Nov => '11', Dec => '12'
+my %months = (
+    Jan => '01',
+    Feb => '02',
+    Mar => '03',
+    Apr => '04',
+    May => '05',
+    Jun => '06',
+    Jul => '07',
+    Aug => '08',
+    Sep => '09',
+    Oct => '10',
+    Nov => '11',
+    Dec => '12'
 );
 
 # standard regexes
@@ -90,7 +94,7 @@ my %re = (
 		| /perl-?5\.004 
 		| /perl_mlb\.zip 
     )}xi,
-    archive => qr{\.(?:tar\.(?:bz2|gz|Z)|t(?:gz|bz)|(?<!ppm\.)zip|pm.gz)$}i,
+    archive    => qr{\.(?:tar\.(?:bz2|gz|Z)|t(?:gz|bz)|(?<!ppm\.)zip|pm.gz)$}i,
     target_dir => qr{
         ^(?:
             modules/by-module/[^/]+/./../ | 
@@ -113,8 +117,8 @@ $re{split_them} = qr{^(.+?)$re{version_suffix}$};
 # and not other "AUTHOR/subdir/whatever"
 
 # Just get AUTHOR/tarball.suffix from whatever file name is passed in
-sub _get_base_id { 
-    my $file = shift;
+sub _get_base_id {
+    my $file    = shift;
     my $base_id = $file;
     $base_id =~ s{$re{target_dir}}{};
     return $base_id;
@@ -123,7 +127,7 @@ sub _get_base_id {
 sub _base_name {
     my ($base_id) = @_;
     my $base_file = basename $base_id;
-    my ($base_name, $base_version) = $base_file =~ $re{split_them};
+    my ( $base_name, $base_version ) = $base_file =~ $re{split_them};
     return $base_name;
 }
 
@@ -134,76 +138,79 @@ sub _base_name {
 #--------------------------------------------------------------------------#-
 
 sub _parse_module_index {
-    my ($self, $packages, $file_ls ) = @_;
+    my ( $self, $packages, $file_ls ) = @_;
 
-	# first walk the packages list
+    # first walk the packages list
     # and build an index
 
-    my (%valid_bases, %valid_distros, %mirror);
-    my (%latest, %latest_dev);
+    my ( %valid_bases, %valid_distros, %mirror );
+    my ( %latest, %latest_dev );
 
-    my $gz = Compress::Zlib::gzopen($packages, "rb")
-        or die "Cannot open package list: $Compress::Zlib::gzerrno";
+    my $gz = Compress::Zlib::gzopen( $packages, "rb" )
+      or die "Cannot open package list: $Compress::Zlib::gzerrno";
 
-    $self->trace( "Scanning 02packages.details ...\n" );
+    $self->trace("Scanning 02packages.details ...\n");
     my $inheader = 1;
-    while ($gz->gzreadline($_) > 0) {
+    while ( $gz->gzreadline($_) > 0 ) {
         if ($inheader) {
             $inheader = 0 unless /\S/;
             next;
         }
 
-        my ($module, $version, $path) = split;
-        next if $self->_filter_module({
+        my ( $module, $version, $path ) = split;
+        next
+          if $self->_filter_module(
+            {
                 module  => $module,
                 version => $version,
                 path    => $path,
-            });
-        
+            }
+          );
+
         my $base_id = _get_base_id("authors/id/$path");
         $valid_distros{$base_id}++;
-        my $base_name = _base_name( $base_id );
+        my $base_name = _base_name($base_id);
         if ($base_name) {
             $latest{$base_name} = {
                 datetime => 0,
-                base_id => $base_id
+                base_id  => $base_id
             };
         }
     }
 
-#    use DDS;
-#    $self->trace("Distros\n");
-#    Dump \%valid_distros;
-#    $self->trace("Bases\n");
-#    Dump \%valid_bases;
+    #    use DDS;
+    #    $self->trace("Distros\n");
+    #    Dump \%valid_distros;
+    #    $self->trace("Bases\n");
+    #    Dump \%valid_bases;
 
     # next walk the find-ls file
     local *FH;
     tie *FH, 'CPAN::Tarzip', $file_ls;
 
-    $self->trace( "Scanning find-ls ...\n" );
-    while ( defined ( my $line = <FH> ) ) {
+    $self->trace("Scanning find-ls ...\n");
+    while ( defined( my $line = <FH> ) ) {
         my %stat;
-        @stat{qw/inode blocks perms links owner group size datetime name linkname/}
-            = split q{ }, $line;
-        
-        unless ($stat{name} && $stat{perms} && $stat{datetime}) {
+        @stat{qw/inode blocks perms links owner group size datetime name linkname/} =
+          split q{ }, $line;
+
+        unless ( $stat{name} && $stat{perms} && $stat{datetime} ) {
             $self->trace("Couldn't parse '$line' \n");
             next;
         }
         # skip directories, symlinks and things that aren't a tarball
-        next if $stat{perms} eq "l" || substr($stat{perms},0,1) eq "d";
+        next if $stat{perms} eq "l" || substr( $stat{perms}, 0, 1 ) eq "d";
         next unless $stat{name} =~ $re{target_dir};
         next unless $stat{name} =~ $re{archive};
 
-        # skip if not AUTHOR/tarball 
+        # skip if not AUTHOR/tarball
         # skip perls
-        my $base_id = _get_base_id($stat{name});
-        next unless $base_id; 
-        
+        my $base_id = _get_base_id( $stat{name} );
+        next unless $base_id;
+
         next if $base_id =~ $re{perls};
 
-        my $base_name = _base_name( $base_id );
+        my $base_name = _base_name($base_id);
 
         # if $base_id matches 02packages, then it is the latest version
         # and we definitely want it; also update datetime from the initial
@@ -212,13 +219,13 @@ sub _parse_module_index {
             $mirror{$base_id} = $stat{datetime};
             next unless $base_name;
             if ( $stat{datetime} > $latest{$base_name}{datetime} ) {
-                $latest{$base_name} = { 
-                    datetime => $stat{datetime}, 
-                    base_id => $base_id
+                $latest{$base_name} = {
+                    datetime => $stat{datetime},
+                    base_id  => $base_id
                 };
             }
         }
-        # if not in the packages file, we only want it if it resembles 
+        # if not in the packages file, we only want it if it resembles
         # something in the package file and we only the most recent one
         else {
             # skip if couldn't parse out the name without version number
@@ -230,9 +237,9 @@ sub _parse_module_index {
             # keep only the latest
             $latest_dev{$base_name} ||= { datetime => 0 };
             if ( $stat{datetime} > $latest_dev{$base_name}{datetime} ) {
-                $latest_dev{$base_name} = { 
-                    datetime => $stat{datetime}, 
-                    base_id => $base_id
+                $latest_dev{$base_name} = {
+                    datetime => $stat{datetime},
+                    base_id  => $base_id
                 };
             }
         }
@@ -243,50 +250,50 @@ sub _parse_module_index {
         my $base_id = $latest{$name}{base_id};
         $mirror{$base_id} = $latest{$name}{datetime} unless $mirror{$base_id};
     }
-          
+
     # for dev versions, it must be newer than the latest version of
     # the same base name from the packages file
 
     for my $name ( keys %latest_dev ) {
-        if ( ! $latest{$name} ) {
-            $self->trace( "Shouldn't be missing '$name' matching '$latest_dev{$name}{base_id}'\n" );
+        if ( !$latest{$name} ) {
+            $self->trace(
+                "Shouldn't be missing '$name' matching '$latest_dev{$name}{base_id}'\n");
             next;
         }
         next if $latest{$name}{datetime} > $latest_dev{$name}{datetime};
-        $mirror{ $latest_dev{$name}{base_id} } = $latest_dev{$name}{datetime} 
+        $mirror{ $latest_dev{$name}{base_id} } = $latest_dev{$name}{datetime};
     }
 
     my $mirror_list =
-        [ sort map { s{^(((.).).+)$}{authors/id/$3/$2/$1}; $_ } keys %mirror ];
+      [ sort map { s{^(((.).).+)$}{authors/id/$3/$2/$1}; $_ } keys %mirror ] ## no critic
+      ;
 
     return $mirror_list;
 }
 
-1; #modules must return true
+1;
 
 __END__
 
-#--------------------------------------------------------------------------#
-# pod documentation 
-#--------------------------------------------------------------------------#
+=pod
 
-=begin wikidoc
+=encoding UTF-8
 
-= NAME
+=head1 NAME
 
 CPAN::Mini::Devel - Create CPAN::Mini mirror with developer releases
 
-= VERSION
+=head1 VERSION
 
-This documentation describes version %%VERSION%%.
+version 0.04
 
-= SYNOPSIS
+=head1 SYNOPSIS
 
     $ minicpan -c CPAN::Mini::Devel
 
-= DESCRIPTION
+=head1 DESCRIPTION
 
-Normally, [CPAN::Mini] creates a minimal CPAN mirror with the latest version of
+Normally, L<CPAN::Mini> creates a minimal CPAN mirror with the latest version of
 each distribution, but excluding developer releases (those with an underscore
 in the version number, like 0.10_01).  
 
@@ -302,49 +309,52 @@ name already in the normal CPAN packages list.
 There may be errors retrieving very new modules if they are indexed but not
 yet synchronized on the mirror.
 
-CPAN::Mini::Devel also mirrors the {indices/find-ls.gz} file, which is used
+CPAN::Mini::Devel also mirrors the F<indices/find-ls.gz> file, which is used
 to identify developer releases.
 
-= USAGE
+=head1 USAGE
 
-See [Mini::CPAN].
+See L<Mini::CPAN>.
 
-= BUGS
+=head1 SEE ALSO
 
-Please report any bugs or feature using the CPAN Request Tracker.  
-Bugs can be submitted through the web interface at 
-[http://rt.cpan.org/Dist/Display.html?Queue=CPAN-Mini-Devel]
+=over 4
 
-When submitting a bug or request, please include a test-file or a patch to an
-existing test-file that illustrates the bug or desired feature.
+=item *
 
-= SEE ALSO
+L<CPAN::Mini>
 
-* [CPAN::Mini]
+=back
 
-= AUTHOR
+=for :stopwords cpan testmatrix url annocpan anno bugtracker rt cpants kwalitee diff irc mailto metadata placeholders metacpan
 
-David A. Golden (DAGOLDEN)
+=head1 SUPPORT
 
-= COPYRIGHT AND LICENSE
+=head2 Bugs / Feature Requests
 
-Copyright (c) 2008 by David A. Golden
+Please report any bugs or feature requests through the issue tracker
+at L<https://github.com/dagolden/CPAN-Mini-Devel/issues>.
+You will be notified automatically of any progress on your issue.
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at 
-[http://www.apache.org/licenses/LICENSE-2.0]
+=head2 Source Code
 
-Files produced as output though the use of this software, shall not be
-considered Derivative Works, but shall be considered the original work of the
-Licensor.
+This is open source software.  The code repository is available for
+public review and contribution under the terms of the license.
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+L<https://github.com/dagolden/CPAN-Mini-Devel>
 
-=end wikidoc
+  git clone https://github.com/dagolden/CPAN-Mini-Devel.git
+
+=head1 AUTHOR
+
+David Golden <dagolden@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is Copyright (c) 2014 by David Golden.
+
+This is free software, licensed under:
+
+  The Apache License, Version 2.0, January 2004
 
 =cut
